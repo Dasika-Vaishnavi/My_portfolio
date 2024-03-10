9ce3f930e94a9e3e77b6caa9498147470dd44914
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-10 18:27:19 +0530

    updated transitio

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,30 @@
+import { motion } from 'framer-motion';
+
+const transitionVariants = {
+  initial: {
+    X: '100%',
+    width: '100%',
+    },
+  animate: {
+    X: '0%',
+    width: '0%',
+    },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%'],
+    },
+}
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]'
+      Variants={transitionVariants}
+      initial='initial'
+      animate='animate'
+      exit='exit'
+      transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+      ></motion.div>
     </>
   );
 };
