fb7a0ed02ca0d7b947e85bc6f9d906445cd3f0cf
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-10 15:41:19 +0530

    made changes in layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,21 @@
-const Layout = () => {
-  return <div>Layout</div>;
+import {Sora} from '@next/font/google';
+import Nav from '../components/Nav';
+import Header from '../components/Header';
+import TopLefting from '../components/TopLeftImg'
+
+const sora = Sora ({
+  subsets: ['latin'],
+  variable: '--font-sora',
+  weight: ['100','200','300','400','500','600','700','800',]
+})
+
+const Layout = ({children}) => {
+  return <div>
+    <Nav />
+    <Header />
+    <TopLefting />
+    {children}
+  </div>;
 };
 
 export default Layout;
