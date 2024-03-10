dd941976cac760e9ec0d5cea6206fd82c72a5308
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-11 01:49:29 +0530

    particles logic added

--- pages/index.js
+++ pages/index.js
@@ -49,12 +49,13 @@ const Home = () => {
               translate-z-0'
               >   
           </div>
-          <div>Particles</div>
+          <ParticlesContainer />
           <motion.div 
-              variants={fadeIn('down', 0.4)} 
-              initial='hidden' 
-              animate='show' 
+              variants={fadeIn('up', 0.5)} 
+              initial='hidden'
+              animate='show'
               exit='hidden'
+              transition={{duration: 1, ease: 'easeInOut'}}
               className='w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]'
               >
               <Avatar />
