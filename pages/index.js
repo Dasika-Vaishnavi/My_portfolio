dc63709159773d058d82494b8abae77aae3deba8
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-11 18:02:14 +0530

    particles fixed

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,8 @@ const Home = () => {
   <div className='bg-primary/60 h-full '>
       <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
           <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
-              <motion.h1 variants={fadeIn('down', 0.4)} 
+              <motion.h1 
+                  variants={fadeIn('down', 0.2)} 
                   initial='hidden' 
                   animate='show' 
                   exit='hidden'
@@ -22,7 +23,8 @@ const Home = () => {
                   >
                       Transforming Ideas <br/> into <span className='text-accent'>Digital Reality</span>
               </motion.h1>
-              <motion.p variants={fadeIn('down', 0.3)} 
+              <motion.p 
+                  variants={fadeIn('down', 0.3)} 
                   initial='hidden' 
                   animate='show' 
                   exit='hidden' 
@@ -45,10 +47,9 @@ const Home = () => {
           </div>
       </div>
       <div className='w-[1200px] h-full absolute right-0 bottom-0'>
-          <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full mix-blend-color-dodge 
+          <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge 
               translate-z-0'
-              >   
-          </div>
+              ></div>
           <ParticlesContainer />
           <motion.div 
               variants={fadeIn('up', 0.5)} 
