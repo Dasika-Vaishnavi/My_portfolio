ecabb1e2c0e229e21e8ad32f7a15d9240b13bd09
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-11 19:01:14 +0530

    updated transition

--- components/Transition.js
+++ components/Transition.js
@@ -31,7 +31,7 @@ const Transition = () => {
       initial='initial'
       animate='animate'
       exit='exit'
-      transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+      transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}
       ></motion.div>
 
       <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]'
@@ -39,7 +39,7 @@ const Transition = () => {
       initial='initial'
       animate='animate'
       exit='exit'
-      transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}
+      transition={{delay: 0.6, duration: 0.6, ease: 'easeInOut'}}
       ></motion.div>
     </>
   );
