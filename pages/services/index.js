58e023cc3fb07bec1384e25cd2a8372652c21a95
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-12 19:57:51 +0530

    added content to services

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,41 @@
+import {} from 'react-icons/rx'
+
+
+export const serviceData = [
+
+]
+
+
+import ServiceSlider from '../../components/ServiceSlider'
+import Bulb from '../../components/Bulb'
+import Circles from '../../components/Circles'
+
+import {motion} from 'framer-motion'
+import {fadeIn} from  "../../variants"
+
 const Services = () => {
-  return <div>Services</div>;
-};
+  return (
+    <div className='h-full bg-primary/30 py-36 flex items-center'>
+       <Circles />
+       <div className='container mx-auto'>
+          <div>
+              {/* text */}
+              <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+                  <h2 className='h2 xl:mt-8'>  
+                      My Services <span className='text-accent'>.</span>
+                  </h2>
+                  <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+                      I specialize in creating custom web applications and websites that are not only visually stunning 
+                      Here are some of the services I offer. YouI offer the following services. Please feel free to 
+                      I offer the following services. Please feel free to
+                  </p>
+                  <ServiceSlider />
+              </div>
+          </div>
+          <Bulb />
+       </div>
+    </div>)
+
+}
 
 export default Services;
