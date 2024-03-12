6b5d03aab2e72fe4cb604996844acd5b5e99bc43
Ankur Pratap Singh <ankur_s@rocketmail.com>
2024-03-12 23:00:59 +0530

    added slider to services

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,11 +1,3 @@
-import {} from 'react-icons/rx'
-
-
-export const serviceData = [
-
-]
-
-
 import ServiceSlider from '../../components/ServiceSlider'
 import Bulb from '../../components/Bulb'
 import Circles from '../../components/Circles'
@@ -18,18 +10,36 @@ const Services = () => {
     <div className='h-full bg-primary/30 py-36 flex items-center'>
        <Circles />
        <div className='container mx-auto'>
-          <div>
-              {/* text */}
+          <div className='flex flex-col xl:flex-row gap-x-8'>
               <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
-                  <h2 className='h2 xl:mt-8'>  
+                  <motion.h2 
+                  variants = {fadeIn('up', 0.2)}
+                  initial = 'hidden'
+                  animate = 'show'
+                  className = 'h2 xl:mt-8'
+                  exit = 'hiddem'
+                  >  
                       My Services <span className='text-accent'>.</span>
-                  </h2>
-                  <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+                  </motion.h2>
+                  <motion.p 
+                  variants = {fadeIn('up', 0.4)}
+                  initial = 'hidden'
+                  animate = 'show'
+                  exit = 'hiddem' 
+                  className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+                  >
                       I specialize in creating custom web applications and websites that are not only visually stunning 
-                      Here are some of the services I offer. YouI offer the following services. Please feel free to 
-                      I offer the following services. Please feel free to
-                  </p>
-                  <ServiceSlider />
+                      Here are some of the services I offer. YouI offer the following services. 
+                  </motion.p>
+                  <div 
+                  variants = {fadeIn('down', 0.6)}
+                  initial = 'hidden'
+                  animate = 'show'
+                  exit = 'hiddem' 
+                  className='w-full xl:max-w-[65%]'
+                  >
+                        <ServiceSlider />
+                  </div>
               </div>
           </div>
           <Bulb />
